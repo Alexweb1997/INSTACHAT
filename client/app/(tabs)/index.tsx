2d33d38dd425c5a90aa88bcffe8e2a80c58d54b4
CodeByAlex <Onyeisiuchenna@gmail.com>
2026-07-05 10:06:25 +0200

    feat: implement initial layout and data fetching logic for MessagesScreen

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,10 +1,47 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Conversation, UserStory } from '@/types'
+import { useRouter } from 'expo-router';
+import { dummyConversationData } from '@/assets/assets';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '@/assets/styles/MessagesScreen.styles'
 
-export default function index() {
+
+
+export default function MessagesScreen() {
+
+    const [conversations, setConversations] = useState<Conversation[]>([]);
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [selectedStory, setSelectedStory] = useState<UserStory | null>(null);
+
+    const router = useRouter()
+
+    const fetchConversations = async () => {
+        setLoading(true)
+        setTimeout(() => {
+            setConversations(dummyConversationData as any)
+            setLoading(false)
+        }, 1000)
+    }
+
+    useEffect(() => {
+        fetchConversations()
+    }, [])
+
     return (
-        <View>
-            <Text>tabs index</Text>
-        </View>
+        <SafeAreaView style={styles.safe} edges={["top"]}>
+
+            {/* header */}
+
+            {/*search */}
+
+            {/*Stories */}
+
+            {/*Divider */}
+
+
+
+        </SafeAreaView>
     )
 }
